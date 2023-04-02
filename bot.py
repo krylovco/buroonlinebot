1285f2559a1f382cb2c4505af1a5e0237f59e279
krylovco <70950219+krylovco@users.noreply.github.com>
2023-04-02 17:18:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
-import telegram
+    import telegram
 from telegram.ext import Updater, CommandHandler
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот!")
+    context.bot.send_message(chat_id=update.message.chat_id, text='Hello, I am your bot!')
 
 def main():
-    bot_token = "6225159605:AAHvu7ldYN5tLcHFG56i1Pu1_FQfWa7vxzM"
-    updater = Updater(bot_token, use_context=True)
+    bot_token = '6225159605:AAHvu7ldYN5tLcHFG56i1Pu1_FQfWa7vxzM'
+    updater = Updater(bot_token)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler('start', start))
     updater.start_polling()
     updater.idle()
 
