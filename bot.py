14629ee936e5e2a936de2e9f5dc05b4a3ceb8d36
krylovco <70950219+krylovco@users.noreply.github.com>
2023-04-02 17:48:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,4 +4,4 @@ bot_token = "6225159605:AAHvu7ldYN5tLcHFG56i1Pu1_FQfWa7vxzM"
 bot = telegram.Bot(token=bot_token)
 
 # Send a test message to yourself
-bot.send_message(chat_id="472346381", text="Hello from your bot!")
+await bot.send_message(chat_id="472346381", text="Hello from your bot!")
